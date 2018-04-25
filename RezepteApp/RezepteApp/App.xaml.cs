10c58925e3026adb2d7b26b1cfb5c7524e50954d
Eugen Richter <android@webducer.de>
2018-04-25 22:43:49 +0200

    Stand 25.04.2018

---
+++ RezepteApp/RezepteApp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using RezepteApp.i18n;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: NeutralResourcesLanguage("en")]
+
+namespace RezepteApp
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new RezepteApp.MainPage());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ RezepteApp/RezepteApp/i18n/TranslationExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RezepteApp.i18n
+{
+    [ContentProperty(nameof(Key))]
+    public class TranslationExtension : IMarkupExtension
+    {
+        public string Key { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrWhiteSpace(Key))
+            {
+                return string.Empty;
+            }
+
+            var translation = AppResources.ResourceManager.GetString(Key);
+
+            if (translation == null)
+            {
+#if DEBUG
+                translation = $"[_{Key}_]";
+#else
+                translation = Key;
+#endif
+            }
+
+            return translation;
+        }
+    }
+}
